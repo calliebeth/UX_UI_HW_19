7b976bf24eb6ac7939309b58a7401612ac99f639
calliebeth <133823028+calliebeth@users.noreply.github.com>
2023-10-14 14:57:20 -0500

    Initial Setup
    Setting up the HTML & CSS before jQuery

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+console.log("Index is loading correctly!");
+
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+      $("#panel2").slideDown("slow");
+    });
+  });
